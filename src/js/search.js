9be7ceb23b066df902a3219c8df5fadab8aee5be
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-05-21 17:06:59 +0300

    +

--- src/js/search.js
+++ src/js/search.js
@@ -3,7 +3,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const API_KEY = "36502661-e8ee83efff2e99e0261d33261";
 const URL = "https://pixabay.com/api/";
-const quantityPage = 10;
+const quantityPage = 40;
 
 const options = new URLSearchParams({
     per_page: quantityPage,
